af0c6aedf818ed304fc3ce20313d913c6c58202e
Kerim Arnaberdiyev <kerim.arnaberdiyev@gmail.com>
2018-08-08 21:35:39 +0300

    Добавил подсчет полетов

--- strategy/src/behavior/fly/FlyWithWings.kt
+++ strategy/src/behavior/fly/FlyWithWings.kt
@@ -3,7 +3,9 @@ package behavior.fly
 import behavior.fly.IFlyBehavior
 
 class FlyWithWings : IFlyBehavior {
+    private var _flightCounter = 0
+
     override fun fly() {
-        println("I'm fly with wings!")
+        println("I'm fly with wings! Flight number ${++_flightCounter}")
     }
 }
