477d8293b659301b8960594a741dc55753a87d06
Kerim Arnaberdiyev <kerim.arnaberdiyev@gmail.com>
2018-09-20 21:13:04 +0300

    Добавлено безопасное уведомление наблюдателей и очищение мапы, если нет активного наблюдателя

--- observer/observer/CObservable.h
+++ observer/observer/CObservable.h
@@ -13,21 +13,22 @@ public:
 
 	void RegisterObserver(ObserverType & observer, unsigned int priority = 0) override
 	{
-		auto it = m_observers.find(priority);
-		if (it == m_observers.end())
+		auto it = m_observersMap.find(priority);
+		if (it == m_observersMap.end())
 		{
-			m_observers.insert(std::pair<unsigned, std::vector<ObserverType *>>(priority, { &observer }));
+			m_observersMap.insert(std::pair<unsigned, std::vector<ObserverType *>>(priority, { &observer }));
 		}
 		else
 		{
-			m_observers.at(priority).push_back(&observer);
+			m_observersMap.at(priority).push_back(&observer);
 		}
 	}
 
 	void NotifyObservers() override
 	{
 		T data = GetChangedData();
-		for (auto it = m_observers.begin(); it != m_observers.end(); ++it)
+		const auto clone = m_observersMap;
+		for (auto it = clone.begin(); it != clone.end(); ++it)
 		{
 			for (auto & observer : it->second)
 			{
@@ -38,10 +39,18 @@ public:
 
 	void RemoveObserver(ObserverType & observer) override
 	{
-		for (auto it = m_observers.begin(); it != m_observers.end(); ++it)
+		for (auto it = m_observersMap.begin(); it != m_observersMap.end(); )
 		{
 			auto & observers = it->second;
 			observers.erase(std::remove(observers.begin(), observers.end(), &observer), observers.end());
+			if (observers.empty())
+			{
+				it = m_observersMap.erase(it);
+			}
+			else
+			{
+				++it;
+			}
 		}
 	}
 
@@ -51,5 +60,5 @@ protected:
 	virtual T GetChangedData()const = 0;
 
 private:
-	std::map<unsigned, std::vector<ObserverType *>, std::greater<unsigned>> m_observers;
+	std::map<unsigned, std::vector<ObserverType *>, std::greater<unsigned>> m_observersMap;
 };
